eaec553e7159d7edd2d1a2ec3cccb96cf3e37f93
leonardochen01 <leonardochen.lc@gmail.com>
2023-04-15 18:34:01 +0200

    added: MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -29,7 +29,16 @@ createApp({
     //scrivo funzione per rimuovere una task. Utilizzo lo splice per rimuovere oggetto dall'array, prendendo indice e numero di elementi da rimuovere.
     removeTask(index){
       this.tasks.splice(index,1)
-    } 
+    } ,
+
+    //scrivo funzione per aggiungere una task. La funzione aggiunge un nuovo oggetto chiamato newTask con lo status "false". newTaskString è una variabile che si aggiorna ogni volta che l'utente inserisce qualcosa nell'input testuale. Unshift viene utilizzato per inserire questo nuovo object nell'array this.tasks
+    addTask(){
+      const newTask = {
+        text: this.newTaskString,
+        status: false
+      }
+      this.tasks.unshift(newTask);
+    }
 
   }
 }).mount('#app')
